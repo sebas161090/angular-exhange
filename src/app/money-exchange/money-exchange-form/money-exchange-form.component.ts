72d7bc9fe75f0ddad89b67b7a9b76c59ce46f610
sebas161090 <sebas.pujalt16@gmail.com>
2019-10-07 20:29:51 -0500

    v1

---
+++ src/app/money-exchange/money-exchange-form/money-exchange-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-money-exchange-form',
+  templateUrl: './money-exchange-form.component.html',
+  styleUrls: ['./money-exchange-form.component.css']
+})
+export class MoneyExchangeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
